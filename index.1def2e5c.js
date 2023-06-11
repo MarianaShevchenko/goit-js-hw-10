a636fa496583ffd69c2d1fcf30ed6e3c53c4646f
MarianaShevchenko <121828202+MarianaShevchenko@users.noreply.github.com>
2023-06-11 17:33:27 +0000

    Deploying to gh-pages from @ MarianaShevchenko/goit-js-hw-10@360470748e9e816bebef0ffed31d9de266f9ef0c 🚀

---
+++ index.1def2e5c.js
@@ -0,0 +1,2 @@
+fetch("https://api.thecatapi.com/v1/breeds",{heders:{Authorization:"live_ rbLLAVWafVoyBQow80r0b39YZ5x2Bn EGCiMfX2yBZpGhlmYCi3WFELkIRBos Pf2u"}}).then((e=>e.json())).then((e=>{console.log(e)}));const e=document.querySelector(".value-id");console.log(e);
+//# sourceMappingURL=index.1def2e5c.js.map
---
+++ index.84e9c683.js
@@ -0,0 +1,2 @@
+!function(){fetch("https://api.thecatapi.com/v1/breeds",{heders:{Authorization:"live_ rbLLAVWafVoyBQow80r0b39YZ5x2Bn EGCiMfX2yBZpGhlmYCi3WFELkIRBos Pf2u"}}).then((function(o){return o.json()})).then((function(o){console.log(o)}));var o=document.querySelector(".value-id");console.log(o)}();
+//# sourceMappingURL=index.84e9c683.js.map
